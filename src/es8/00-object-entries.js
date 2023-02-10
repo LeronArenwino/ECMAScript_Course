42cea0a1db6cde39982e6b39320338ee1447077d
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-09 21:16:38 -0500

    :zap: Add result comments

--- src/es8/00-object-entries.js
+++ src/es8/00-object-entries.js
@@ -1,2 +1,10 @@
 const countries = {MX: 'Mexico', CO: 'Colombia', CL: 'Chile', PE: 'Peru'};
 console.log(Object.entries(countries));
+
+// Result
+// [
+//  [ 'MX', 'Mexico' ],
+//  [ 'CO', 'Colombia' ],
+//  [ 'CL', 'Chile' ],
+//  [ 'PE', 'Peru' ]
+// ]
--- src/es8/01-object-values.js
+++ src/es8/01-object-values.js
@@ -1,2 +1,5 @@
 const countries = { MX: "Mexico", CO: "Colombia", CL: "Chile", PE: "Peru" };
 console.log(Object.values(countries));
+
+// Result
+// [ 'Mexico', 'Colombia', 'Chile', 'Peru' ]
