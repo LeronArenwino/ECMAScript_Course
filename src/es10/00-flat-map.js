34093581e1274d11528ebf3196f7880839939f76
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-10 16:14:33 -0500

    :zap: Flat map

---
+++ src/es10/00-flat-map.js
@@ -0,0 +1,11 @@
+const array = [1, 1, 2, 3, 4, [1, 3, 5, 6, [1, 2, 4]]];
+console.log(array.flat(3));
+
+// Result
+// [1, 1, 2, 3, 4, 1, 3, 5, 6, 1, 2, 4]
+
+const array2 = [1, 2, 3, 4, 5];
+console.log(array2.flatMap(v => [v, v * 2]));
+
+// Result
+// [1, 2, 2, 4, 3, 6, 4, 8, 5, 10]
