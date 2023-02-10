144d20a793c755af44003473d23847b7a6bf3ce9
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-10 11:29:51 -0500

    :zap: Async

---
+++ src/es9/03-async.js
@@ -0,0 +1,32 @@
+async function* anotherGenerator() {
+  yield await Promise.resolve(1);
+  yield await Promise.resolve(2);
+  yield await Promise.resolve(3);
+}
+
+const other = anotherGenerator();
+other.next().then((response) => console.log(response.value));
+other.next().then((response) => console.log(response.value));
+other.next().then((response) => console.log(response.value));
+console.log('Hello!');
+
+// Result
+// Hello!
+// 1
+// 2
+// 3
+
+async function arrayOfNames(array){
+    for await (let value of array){
+        console.log(value);
+    }
+}
+
+const names = arrayOfNames(['Léron', 'Francisco', 'Ana']);
+console.log('After');
+
+// Result
+// After
+// Léron
+// Francisco
+// Ana
