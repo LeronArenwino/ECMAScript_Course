f5d629aae3bf70b32faa0a1e8bbd8dc01f7ca400
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-10 17:45:54 -0500

    :zap: ReplaceAll

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,6 @@
+const string = 'Javascript es un maravilloso lenguaje de programación';
+const replaceString = string.replace('Javascript', 'Typescript');
+console.log(replaceString);
+
+// Result
+// Typescript es un maravilloso lenguaje de programación
