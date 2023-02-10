186da55f0493e4f903708cd0c7941d2f92c86f7f
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-10 17:05:29 -0500

    :zap: BigInt

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,8 @@
+const aBigNumber = 1231512312312312n;
+const anotherBigNumber = BigInt(1231512312312312);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+
+// Result
+// 1231512312312312n;
+// 1231512312312312n;
